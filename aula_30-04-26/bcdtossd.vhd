5ed1f9b0935bd78bd0927401cf5c6086dadd82de
Yan Vitor Pires Pereira <yanvitor.pp@gmail.com>
2026-04-30 09:24:42 -0300

    aula dia 30/04/2026

---
+++ aula_30-04-26/bcdtossd.vhd
@@ -0,0 +1,34 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity bcd2ssd is
+
+	port (
+			sw : in std_logic_vector(3 downto 0);
+			hex0: out bit_vector (6 downto 0)
+			);
+end bcd2ssd;
+
+architecture code_v1 of bcd2ssd is
+begin
+	process(sw)
+		begin
+			case sw is					 --abcdefg
+				when "0000" => hex0 <= "0000001";
+				when "0001" => hex0 <= "1001111";
+				when "0010" => hex0 <= "0010010";
+				when "0011" => hex0 <= "0000110";
+				when "0100" => hex0 <= "1001100";
+				when "0101" => hex0 <= "0100100";
+				when "0110" => hex0 <= "0100000";
+				when "0111" => hex0 <= "0001111";
+				when "1000" => hex0 <= "0000000";
+				when "1001" => hex0 <= "0001100";
+				when others => hex0 <= "0000110";
+			end case;
+	end process;
+end architecture;
+
+configuration ifsc_v1 of bcd2ssd is
+	for code_v1 end for;
+end configuration;
